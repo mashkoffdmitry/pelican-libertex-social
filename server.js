a1b4a78897942fd74912253c1cbf466d2583e273
mashkovd <mashkovd@live.ru>
2026-05-09 12:28:37 +0200

    Update PKG_VERSION to 0.3.4 to serve pelican-vue@0.3.4

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const url = require('url');
 const zlib = require('zlib');
 const { uploadCatalog } = require('./r2-uploader');
 
-const PKG_VERSION = '0.3.3';
+const PKG_VERSION = '0.3.4';
 const INDEX_HTML = `<!DOCTYPE html>
 <html lang="en">
 <head>
